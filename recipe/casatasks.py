3324475cc7d2a2473f688e5f0040cd682c9e6c43
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2017-10-20 16:28:37 +0100

    First memoized task

--- recipe/casatasks.py
+++ recipe/casatasks.py
@@ -2,10 +2,12 @@
 Hand-wrapped tasks for CASA
 """
 
-import inspect
+import inspect, shutil, os
 from hashlib import sha256
 
 import casa
+
+import repo
 
 def hf(fn, *args, **kwargs):
     """Hash a function call, including function name """
@@ -15,6 +17,18 @@ def clean(*args, **kwargs):
     return hf(casa.clean, *args, **kwargs)
 
 def split(*args, **kwargs):
-    return hf(casa.split, *args, **kwargs)
+    return hf(casa.split, b*args, **kwargs)
 
-    
+def ft(*args, **kwargs):
+    hh=hf(casa.ft, *args, **kwargs)
+    mm=repo.get(hh)
+    if mm:
+        return mm
+
+    aa=inspect.getcallargs(casa.ft, *args, **kwargs)
+    f=repo.mktemp()
+    os.remove(f)
+    shutil.copytree(aa['vis'], f)
+    aa['vis']=f
+    casa.ft(*args, **kwargs)
+    return repo.put(f, hh)
--- recipe/repo.py
+++ recipe/repo.py
@@ -1,13 +1,15 @@
 """
 Repository for recipe values
 
-TODO: switch to mtime. Directory atimes prone to caching peculiarities. 
+TODO: switch to mtime. Directory atimes prone to caching peculiarities.
 """
 import os
 from hashlib import sha256
 import tempfile
 import subprocess
 import shutil
+
+REPODIR="/home/user/temp/casa/repo/"
 
 # Default is 20GB
 EVLIM=20 * (1<<20)
@@ -16,7 +18,7 @@ def atimes(d):
     """
     Returns in ascending order (so least recently accessed is first)
     """
-    
+
     res=[]
     def ww(fl):
         for f in fl:
@@ -46,3 +48,28 @@ def evict_m(d):
         while(len(al) and df(d)<EVLIM):
             x=al.pop(0)[1]
             print "Removing" , x
+
+def get(h):
+    p=os.path.join(REPODIR, h)
+    if os.path.exists(p):
+        return p
+    else:
+        return False
+
+def mktemp():
+    fd,n=tempfile.mkstemp(prefix="recipetmp", dir=REPODIR)
+    os.close(fd)
+    return n
+
+def put(d, h):
+    """Put file d at hash h
+
+    :param d: the file to put, it is moved, so won't exist at end of
+    operation
+    """
+    p=os.path.join(REPODIR, h)
+    if os.path.exists(p):
+        pass
+    else:
+        shutil.move(d, p)
+    return p
