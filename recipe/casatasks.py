f80725f18702395af9de964b39c23f18c42d45f5
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2017-10-20 17:15:58 +0100

    First decorator -- memoization/cache

--- recipe/casatasks.py
+++ recipe/casatasks.py
@@ -1,5 +1,10 @@
 """
 Hand-wrapped tasks for CASA
+
+What we need to do here:
+  - For tasks that modify their input, copy them first
+  - For tasks with multiple outputs, put them all in a directory
+  - For tasks that pickup files from cwd, make sure they don't
 """
 
 import inspect, shutil, os
@@ -8,6 +13,20 @@ from hashlib import sha256
 import casa
 
 import repo
+
+# Decos
+def ccheck(fn):
+    def newfn(*args, **kwargs):
+        hh=hf(getattr(casa, fn.__name__), *args, **kwargs)
+        mm=repo.get(hh)
+        if mm:
+            return mm
+        else:
+            tempf=fn(*args,  **kwargs)
+            return repo.put(tempf, hh)    
+    return newfn
+
+# Main part
 
 def hf(fn, *args, **kwargs):
     """Hash a function call, including function name """
@@ -33,17 +52,13 @@ def ft(*args, **kwargs):
     casa.ft(**aa)
     return repo.put(f, hh)
 
+@ccheck
 def gaincal(*args, **kwargs):
-    hh=hf(casa.gaincal, *args, **kwargs)
-    mm=repo.get(hh)
-    if mm:
-        return mm
-
     aa=inspect.getcallargs(casa.gaincal, *args, **kwargs)
-    if aa.has_key("caltable"):
+    if aa.has_key("caltable") and aa["caltable"] != "" :
         print "Warning: can not supply the output; will be ignored"
     f=repo.mktemp()
     os.remove(f)
     aa['caltable']=f
     casa.gaincal(**aa)
-    return repo.put(f, hh)    
+    return f
